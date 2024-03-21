a582e662a92b77c49a09b62d95d9be547b357f08
juniordbz <franciscojunior02@hotmail.com.br>
2024-03-21 18:00:00 -0300

    component intro

---
+++ src/lib/Intro.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let title: string
+  export let content: string
+  export let btn: string
+  export let href: string
+  export let icon: string
+</script>
+
+<section
+  class="relative flex min-h-[360px] w-full items-center justify-start bg-musculacao bg-cover bg-center pl-10 after:absolute after:inset-0 after:bg-black/50"
+>
+  <div class="z-10">
+    <h1 class="text-4xl">
+      {@html title}
+      <img class="inline h-9 w-9" src={icon} alt="" />
+    </h1>
+    <p class="mt-4 max-w-[450px] text-lg">
+      {content}
+    </p>
+
+    <a
+      class="d mt-6 inline-block rounded-sm border px-2 py-1 font-roboto delay-150 hover:bg-white hover:text-black hover:delay-100"
+      {href}
+      >{btn}
+    </a>
+  </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,12 @@
 <script lang="ts">
+  import Intro from '$lib/Intro.svelte'
   import icon from '$lib/assets/icon.png'
 </script>
 
-<section
-  class="relative flex min-h-[360px] w-full items-center justify-start bg-musculacao bg-cover bg-center pl-10 after:absolute after:inset-0 after:bg-black/50"
->
-  <div class="z-10">
-    <h1 class="text-4xl">
-      Você está pronto<br /> para o desafio?
-      <img class="inline h-9 w-9" src={icon} alt="" />
-    </h1>
-    <p class="mt-4 max-w-[450px] text-lg">
-      A Academia Local está pronta para te ajudar a chegar mais longe e alcançar
-      sua melhor performance.
-    </p>
-
-    <button
-      type="button"
-      class="mt-6 rounded-sm border px-2 font-roboto delay-150 hover:bg-white hover:text-black hover:delay-100"
-    >
-      Modalidades
-    </button>
-  </div>
-</section>
+<Intro
+  title={`Você está pronto </br> para o desafio? `}
+  content="A Academia Local está pronta para te ajudar a chegar mais longe e alcançar sua melhor performance."
+  btn="Sobre"
+  href="/"
+  {icon}
+/>
